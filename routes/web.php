e683861aa5d78a43d275953078a56a353649f608
tansenkhan1990 <tansenkhan1990@gmail.com>
2018-01-03 13:37:56 +0100

    changing route file

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Post;
+use Illuminate\Http\Request;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts', 'PostsController@index')->name('posts.index');
+Route::get('/posts/details/{id}', 'PostsController@details')->name('posts.details');
+Route::get('/posts/add', 'PostsController@add')->name('posts.add');
+Route::post('/posts/insert', 'PostsController@insert')->name('posts.insert');
+Route::get('/posts/edit/{id}', 'PostsController@edit')->name('posts.edit');
+Route::post('/posts/update/{id}', 'PostsController@update')->name('posts.update');
+Route::get('/posts/delete/{id}', 'PostsController@delete')->name('posts.delete');
