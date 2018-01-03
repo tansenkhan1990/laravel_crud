4974e7df540c8a825026773ace21b0173ff15cbd
tansenkhan1990 <tansenkhan1990@gmail.com>
2018-01-03 11:22:46 +0100

    adding essential path in Postscontroller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Session;
+use App\Post;
 
 class PostsController extends Controller
 {
-    //
+
+    
 }
